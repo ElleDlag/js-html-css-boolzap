c9808f07c756f4b668ce85a4566c696fdcd1a865
ElleDlag <zone.by.lg@gmail.com>
2019-11-18 09:45:50 +0100

    delete unused file

--- assets/js/template.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    var myBox = $('#tmpl_msg').html().trim();
-    var myAvatar = $('#tmpl_avatar').html().trim();
-    var mySentBox = ($(myBox)).children('h2').append($(myAvatar).clone())
-    $('body').append($(mySentBox).clone())
-})//close document here
